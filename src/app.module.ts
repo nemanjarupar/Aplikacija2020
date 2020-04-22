6810faf93c5499973ceb2970b166033f88ac156f
Nemanja Rupar <nemanja.rupar.16@singimail.rs>
2020-04-22 23:11:24 +0200

    Generate all remaining datebase models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { TypeOrmModule } from '@nestjs/typeorm;
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConfiguration } from 'config/database.configuration';
+import { Administrator } from 'entities/administrator.entity';
 import { AdministratorService } from './administrator/administrator.service';
+import { from } from 'rxjs';
+import { ArticleFeature } from 'entities/article-feature.entity';
+import { ArticlePrice } from 'entities/article-price.entity';
+import { Article } from 'entities/article.entity';
+import { CartArticle } from 'entities/cart-article.entity';
+import { Cart } from 'entities/cart.entity';
+import { Category } from 'entities/category.entity';
+import { Feature } from 'entities/feature.entity';
+import { Order } from 'entities/order.entity';
+import { Photo } from 'entities/photo.entity';
+import { User } from 'entities/user.entity';
 
 
 @Module({
@@ -14,8 +26,21 @@ import { AdministratorService } from './administrator/administrator.service';
       username: DatabaseConfiguration.username,
       password: DatabaseConfiguration.password,
       database: DatabaseConfiguration.database, 
-      entities: [ ]
-    })
+      entities: [
+        Administrator,
+        ArticleFeature,
+        ArticlePrice,
+        Article,
+        CartArticle,
+        Cart,
+        Category,
+        Feature,
+        Order,
+        Photo,
+        User,
+      ]
+    }),
+    TypeOrmModule.forFeature([ Administrator ])
   ],
   controllers: [AppController],
   providers: [AdministratorService],
