8b813f93ebcf78777221509946b2ef4f05cd97c2
Nemanja Rupar <nemanja.rupar.16@singimail.rs>
2020-08-25 01:35:29 +0200

    upload errors

--- src/controllers/api/article.controller.ts
+++ src/controllers/api/article.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Param, UseInterceptors, UploadedFile, Req } from "@nestjs/common";
 import { Crud } from "@nestjsx/crud";
 import { ArticleService } from "src/services/article/article.service";
 import { Article } from "entities/article.entity";
 import { AddArticleDto } from "src/dtos/article/add.article.dto";
+import { FileInterceptor } from '@nestjs/platform-express';
+import { StorageConfig } from "config/storage.config";
+import { diskStorage } from "multer";
+import { PhotoService } from "src/services/photo/photo.service";
+import { Photo } from "entities/photo.entity";
+import { ApiResponse } from "src/misc/api.response.class";
 
 @Controller('api/article')
 @Crud({
@@ -38,10 +44,96 @@ import { AddArticleDto } from "src/dtos/article/add.article.dto";
     }
 })
 export class ArticleController {
-    constructor(public service: ArticleService) { }
+    constructor(public service: ArticleService,
+
+                public photoService: PhotoService,
+        ) { }
 
     @Post('createFull')
     createFullArticle(@Body() data: AddArticleDto) {
         return this.service.createFullArticle(data);
+    }
+
+    @Post(':id/uploadPhoto/')
+    @UseInterceptors(
+        FileInterceptor('photo', {
+            storage: diskStorage({
+                destination: StorageConfig.photoDestination,
+                filename: (req, file, callback) => {
+
+                    let original: string = file.originalname;
+                    
+                    let normalized = original.replace(/\s+/g, '-');
+                    normalized = normalized.replace(/[^A-z0-9\.\-]/g, '');
+                    let sada = new Date();
+                    let datePart = '';
+                    datePart += sada.getFullYear().toString();
+                    datePart += (sada.getMonth() + 1).toString();
+                    datePart += sada.getDate().toString();
+
+                    
+                    let randomPart: string = 
+                    new Array(10)
+                    .fill(0)
+                    .map(e => (Math.random() * 9).toFixed(0).toString())
+                    .join('');
+
+                    let fileName = datePart + '-' + randomPart + '-' +normalized;
+
+                    fileName = fileName.toLocaleLowerCase();
+
+                    callback(null, fileName);
+                }
+            }),
+            fileFilter: (req, file, callback) => {
+                if(!file.originalname.toLowerCase().match(/\.(jpg|png)$/)) {
+                    req.fileFilterError = 'Bad file extension!';
+                    callback(null, false);
+                    return;
+                }
+
+                if (!(file.mimetype.includes('jpeg') || file.mimetype.includes('png'))) {
+                    req.fileFilterError = 'Bad file content!';
+                    callback(null, false);
+                    return;
+
+                }
+
+                callback(null, true);
+            },
+            limits: {
+                files: 1,
+                fileSize: StorageConfig.photoMaxFileSize,
+            },
+
+        })
+    )
+    async uploadPhoto(
+        @Param('id') articleId: number,
+         @UploadedFile() photo,
+         @Req() req
+         ): Promise<ApiResponse | Photo> {
+             if (req.fileFilterError) {
+                 return new ApiResponse('error', -4002, req.fileFilterError);
+             }
+
+             if (!photo) {
+                return new ApiResponse('error', -4002, 'File not uploaded!');
+
+             }
+
+        const newPhoto: Photo = new Photo();
+        newPhoto.articleId = articleId;
+        newPhoto.imagePath = photo.filename;
+
+        const savedPhoto = await this.photoService.add(newPhoto);
+        if (!savedPhoto) {
+            return new ApiResponse('error', -4001);
+        }
+
+        
+
+        return savedPhoto; 
+
     }
 }
