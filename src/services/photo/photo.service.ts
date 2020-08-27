50d926ac3d4e292a6fde3b1b054bba1d824bedfc
Nemanja Rupar <nemanja.rupar.16@singimail.rs>
2020-08-27 22:57:22 +0200

    Implement a mechanism to delete article photos

--- src/services/photo/photo.service.ts
+++ src/services/photo/photo.service.ts
@@ -14,4 +14,8 @@ export class PhotoService extends TypeOrmCrudService<Photo> {
         return this.photo.save(newPhoto);
 
     }
+
+    async deleteById(id: number) {
+        return await this.photo.delete(id);
+    }
 }
