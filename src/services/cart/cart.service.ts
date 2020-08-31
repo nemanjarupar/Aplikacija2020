06c7d48e1db6b82010c1ede0fe2afe0f602f52cc
Nemanja Rupar <nemanja.rupar.16@singimail.rs>
2020-08-31 22:16:56 +0200

    Modify the cart presentation logic

--- src/services/cart/cart.service.ts
+++ src/services/cart/cart.service.ts
@@ -14,12 +14,6 @@ export class CartService {
 
         @InjectRepository(CartArticle)
         private readonly cartArticle: Repository<CartArticle>,
-
-        @InjectRepository(Article)
-        private readonly article: Repository<Article>,
-
-        @InjectRepository(Order)
-        private readonly order: Repository<Order>,
     ) { }
 
     async getLastActiveCartByUserId(userId: number): Promise<Cart | null> {
@@ -80,6 +74,7 @@ export class CartService {
                 "cartArticles",
                 "cartArticles.article",
                 "cartArticles.article.category",
+                "cartArticles.article.articlePrices",
 
             ],
         });
